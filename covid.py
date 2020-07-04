c5451afe20aae074629547d64ea66583cb70279e
AlexThe5th <afinlayv@gmail.com>
2020-07-04 18:37:48 -0400

    added colors to graph

--- covid.py
+++ covid.py
@@ -12,28 +12,33 @@ def load_data():
     file.write(data)
     file.close()
 
-    url = f'https://covidtracking.com/api/v1/states/current.json'
+    url = f'https://covidtracking.com/api/v1/states/daily.json'
     uh = urllib.request.urlopen(url)
     data = uh.read().decode()
     file = open('covid_data_ts.json', 'w')
     file.write(data)
     file.close()
 
+    data_ts = pd.read_json('covid_data_ts.json')
+    data_ts['date'] =  pd.to_datetime(data_ts['date'], format='%Y%m%d', errors='ignore')
+
     data_cur = pd.read_json('covid_data_current.json')
+    data_cur['date'] =  pd.to_datetime(data_cur['date'], format='%Y%m%d', errors='ignore')
+
     state_pop = pd.read_csv('SCPRC-EST2019-18+POP-RES.csv')
     state_abrev = pd.read_json('state_abrev.json')
 
-    return data_cur, state_pop, state_abrev
+    return data_ts, data_cur, state_pop, state_abrev
 
 
-def normalize(data_cur, state_pop, state_abrev):
+def normalize(data_ts, data_cur, state_pop, state_abrev):
     #load relavent data
     pop = state_pop.get(['NAME', 'POPESTIMATE2019'])
     cur = data_cur.get(['date','state','positive','negative','death'])
     ab = state_abrev
-
+    ts = data_ts.get(['date','state','positive','negative','death'])
     # Convert date format in daily and current data
-    #cur['date'] =  pd.to_datetime(cur['date'], format='%Y%m%d', errors='ignore')
+    #ts['date'] =  pd.to_datetime(ts['date'], format='%Y%m%d', errors='ignore')
 
 
     # Merge state population data with state abreviations
@@ -45,7 +50,7 @@ def normalize(data_cur, state_pop, state_abrev):
     cur = cur.get(['date','state','POPESTIMATE2019','positive','negative','death'])
     # Merge Population data into daily data using state abreviation data
 
-    return cur
+    return ts, cur
 
 
 def analyse(cur):
@@ -61,24 +66,33 @@ def analyse(cur):
 
     return cur
 
+def analyse_ts(ts):
+    # calculate per 100k for current values
 
-def output(data):
+    return ts
 
-    print(data)
-    data.plot.bar(x='state', y=['positive per 100k'], rot=0, figsize = (14, 14))
+
+def output(data_cur, data_ts):
+
+    print(data_cur, data_ts)
+    data_cur.plot.bar(x='state',y=['mortality %', 'death per 100k'], rot=0, figsize = (14, 14), colormap='Dark2')
+    #data_cur.plot.bar(x='state', y=['mortality %'], c = ['red'], rot=0, figsize = (14, 14))
+
     plt.show()
 
 
-    return data
+    return data_cur, data_ts
 
 
 
 
 def main():
-    data_cur, state_pop, state_abrev = load_data()
-    table = normalize(data_cur, state_pop, state_abrev)
-    data = analyse(table)
-    output(data)
+    data_ts, data_cur, state_pop, state_abrev = load_data()
+    ts, cur = normalize(data_ts, data_cur, state_pop, state_abrev)
+    data_cur = analyse(cur)
+    data_ts = analyse_ts(ts)
+
+    output(data_cur, data_ts)
 
 
 main()
