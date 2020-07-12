33f116428b763a9c0db45871c04c7066d2b840d0
AlexThe5th <afinlayv@gmail.com>
2020-07-12 16:55:18 -0400

    output time series data as dataframes.

--- covid.py
+++ covid.py
@@ -7,8 +7,8 @@ STATE_ABREV='state_abrev.json'
 COVID_DATA_URL='https://covidtracking.com/api/v1/states/daily.json'
 COVID_OUTPUT_FILE_NAME='covid_data.json'
 GET_FIELDS_STATE=['NAME', 'POPESTIMATE2019']
-GET_FIELDS_COVID=['date','state','positive','negative','death', 'positiveIncrease','negativeIncrease', 'totalTestResults']
-ALL_FIELDS=['date','state','POPESTIMATE2019','positive','negative','death', 'positiveIncrease','negativeIncrease','totalTestResults']
+GET_FIELDS_COVID=['date','state','positive','negative','death', 'totalTestResults', 'positiveIncrease','negativeIncrease', 'totalTestResultsIncrease', ]
+ALL_FIELDS=['date','state','POPESTIMATE2019','positive','negative','death', 'totalTestResults', 'positiveIncrease','negativeIncrease','totalTestResultsIncrease']
 OUTPUT_LIST=['mortality %', 'death %']
 STATES=['all']
 if 'all' in STATES:
@@ -19,15 +19,6 @@ if 'all' in STATES:
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 DEBUG=False
 LOAD_NEW_DATA=False
-
-
-def ts(self, state, date_start, date_end, field):
-    ts_data = [self, state, date_start, date_end, field]
-    return ts_data
-
-def cur(self, state):
-    cur_data = [self, state]
-    return cur_data
 
 def debug(message, data):
     if DEBUG == True:
@@ -50,7 +41,7 @@ def load_data_and_save(url, output_file_name):
 def parse_to_pd(data_file_name):
     print(f'Reading data from {data_file_name}...')
     data=pd.read_json(data_file_name)
-    debug('column data:', data.columns)
+    print('column data:', data.columns)
     data['date']=pd.to_datetime(data['date'], format='%Y%m%d')
     state_pop=pd.read_csv(STATE_POP_DATA)
     state_abrev=pd.read_json(STATE_ABREV)
@@ -65,46 +56,46 @@ def load_states(data):
     print('Loading states...')
     state_data_lst=[]
     for state in STATES:
-        print(state)
+        print(state, )
         state_data_lst.append(state_data(data, state))
-    debug('loaded states:', state_data_lst)
+    debug('loaded states:', STATES)
     return state_data_lst
 
 def state_data(data, state):
     state_data= data.query(f'state == "{state}"')
-    debug(f'Loaded state data list({state}): \n', state_data)
+    debug(f'Loaded state data list({state}): \n', type(state_data))
     return state_data
 
 def analyse(data):
+    print('Analysing data...')
     for state in data:
-        state_name = state['state']
-        print(f'Calculating percentages for {state_name}...')
+
         state['positive %'] = (state['positive'] / state['POPESTIMATE2019']) * 100
         state['negative %']  = (state['negative'] / state['POPESTIMATE2019']) * 100
         state['death %'] = (state['death'] / state['POPESTIMATE2019']) * 100
         state['mortality %'] = (state['death'] / state['positive']) * 100
         state['tested %'] = (state['totalTestResults'] / state['POPESTIMATE2019']) * 100
         state['positive/negative %'] = (state['positive'] / state['negative']) * 100
-        state['new test positive %'] = (state['positiveIncrease'] / state['negativeIncrease']) * 100
-        debug(f'Analysed {state} data:', data)
+        state['new test positive %'] = (state['positiveIncrease'] / state['totalTestResultsIncrease']) * 100
+        debug(f'Analysed {state} data:', type(data))
     return data
 
-def output(data):
-    print(data)
+def ts(data, field):
+    data_list = []
+    for i in data:
+        nt_pos = i.get(['date', 'state', field])
+        data_list.append(nt_pos)
+    return data_list
 
-def graph(data, states):
-
-    return data
 
 def run():
 
 
     load_data_and_save(COVID_DATA_URL, COVID_OUTPUT_FILE_NAME)
     data_pd = parse_to_pd(COVID_OUTPUT_FILE_NAME)
-    debug('data type after parse:', type(data_pd))
-    data = load_states(data_pd)
-    data = analyse(data)
-    output(data)
-    graph(data, STATES)
+    data_states = load_states(data_pd)
+    data_analysis = analyse(data_states)
+    data_ts = ts(data_analysis, 'tested %')
+    print(data_ts)
 
 run()
