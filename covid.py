0a2b176059f6c4702815fc3660beb8ec7a353356
AlexThe5th <afinlayv@gmail.com>
2020-07-12 22:02:37 -0400

    rewrite

--- covid.py
+++ covid.py
@@ -17,12 +17,12 @@ if 'all' in STATES:
           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-DEBUG=False
-LOAD_NEW_DATA=False
+DEBUG=True
+LOAD_NEW_DATA=True
 
 def debug(message, data):
     if DEBUG == True:
-        print(message, data)
+        print(message, "\n-----------------------------\n", data, "\n-----------------------------\n")
 
 def load_data_and_save(url, output_file_name):
     if LOAD_NEW_DATA == True:
@@ -34,78 +34,55 @@ def load_data_and_save(url, output_file_name):
         file.close()
         debug('Loaded data:' , data)
         debug('Data saved to:', output_file_name)
+        data=pd.read_json(data)
+        return data
     else:
         print(f'New data not loaded, reading from {output_file_name}. Set LOAD_NEW_DATA=True to load data')
+        data=pd.read_json(data)
+        return data
 
 
-def parse_to_pd(data_file_name):
-    print(f'Reading data from {data_file_name}...')
-    data=pd.read_json(data_file_name)
-    print('column data:', data.columns)
+
+
+def analyse(data):
+
+    show_info(data)
     data['date']=pd.to_datetime(data['date'], format='%Y%m%d')
     state_pop=pd.read_csv(STATE_POP_DATA)
     state_abrev=pd.read_json(STATE_ABREV)
     states=pd.merge(state_abrev, state_pop, left_on="name", right_on="NAME")
     data=pd.merge(states, data.get(GET_FIELDS_COVID), left_on='abbreviation', right_on='state')
     data=data.get(ALL_FIELDS)
-    debug('Parsed data:', data)
-    debug('columns list:', data.columns)
+
+
+    data['positive %'] = (data['positive'] / data['POPESTIMATE2019']) * 100
+    data['negative %']  = (data['negative'] / data['POPESTIMATE2019']) * 100
+    data['death %'] = (data['death'] / data['POPESTIMATE2019']) * 100
+    data['mortality %'] = (data['death'] / data['positive']) * 100
+    data['tested %'] = (data['totalTestResults'] / data['POPESTIMATE2019']) * 100
+    data['positive/negative %'] = (data['positive'] / data['negative']) * 100
+    data['new test positive %'] = (data['positiveIncrease'] / data['totalTestResultsIncrease']) * 100
+
+    show_info(data)
     return data
 
-def load_states(data):
-    print('Loading states...')
-    state_data_lst=[]
-    for state in STATES:
-        print(state, )
-        state_data_lst.append(state_data(data, state))
-    debug('loaded states:', STATES)
-    return state_data_lst
 
-def state_data(data, state):
-    state_data= data.query(f'state == "{state}"')
-    debug(f'Loaded state data list({state}): \n', type(state_data))
-    return state_data
-
-def analyse(data):
-    print('Analysing data...')
-    for state in data:
-        state['positive %'] = (state['positive'] / state['POPESTIMATE2019']) * 100
-        state['negative %']  = (state['negative'] / state['POPESTIMATE2019']) * 100
-        state['death %'] = (state['death'] / state['POPESTIMATE2019']) * 100
-        state['mortality %'] = (state['death'] / state['positive']) * 100
-        state['tested %'] = (state['totalTestResults'] / state['POPESTIMATE2019']) * 100
-        state['positive/negative %'] = (state['positive'] / state['negative']) * 100
-        state['new test positive %'] = (state['positiveIncrease'] / state['totalTestResultsIncrease']) * 100
-        debug(f'Analysed {state} data:', type(data))
-    return data
-
-def ts(data, field):
-    data_list = []
-    for i in data:
-        nt_pos = i.get(['date', 'state', field])
-        data_list.append(nt_pos)
-    return data_list
-
-def cur(data, state):
-    cur_dict = {}
-    # Magic???
-    return cur_dict
 
 def output(data):
     print(data)
     # graph data
 
+def show_info(data):
+        debug('column data:', data.columns)
+        debug('data types:', data.dtypes)
+        debug('DataFrame size:', data.size)
+        debug('DataFrame shape:', data.shape)
+
 def run():
 
 
-    load_data_and_save(COVID_DATA_URL, COVID_OUTPUT_FILE_NAME)
-    data_pd = parse_to_pd(COVID_OUTPUT_FILE_NAME)
-    data_states = load_states(data_pd)
-    data_analysis = analyse(data_states)
-    all_ts_data =[]
-    for field in OUTPUT_LIST:
-        data_ts = ts(data_analysis, field)
-        all_ts_data.append(data_ts)
-    output(all_ts_data)
+    raw_data = load_data_and_save(COVID_DATA_URL, COVID_OUTPUT_FILE_NAME)
+    data_clean = analyse(raw_data)
+    output(data_clean)
 
 run()
